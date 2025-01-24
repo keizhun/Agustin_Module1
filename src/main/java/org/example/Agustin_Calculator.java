89f9c81a9aba3ea74316e44e6880812846994b12
keizhun <ethankeith876@gmail.com>
2025-01-24 18:32:45 +0800

    Commit

--- src/main/java/org/example/Agustin_Calculator.java
+++ src/main/java/org/example/Agustin_Calculator.java
@@ -15,12 +15,12 @@ public class Agustin_Calculator {
 
         sum = number1 + number2;
         sub = number1 - number2;
-        div = number1 / number2;
         mul = number1 * number2;
+        div = number1 / number2;
 
         System.out.println("\n\n" + "Addition: "+sum);
         System.out.println("Subtraction: "+sub);
+        System.out.println("Multiplication: "+mul);
         System.out.println("Division: "+div);
-        System.out.println("Multiplicaiton: "+mul);
     }
 }
